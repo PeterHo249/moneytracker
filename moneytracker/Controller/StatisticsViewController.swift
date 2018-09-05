3cf773aa870a0bbea63e73a366441e684f98b9e3
Peter Ho <peterho@Peters-Mac.local>
2018-09-05 22:12:06 +0700

    Add control for text field

--- moneytracker/Controller/StatisticsViewController.swift
+++ moneytracker/Controller/StatisticsViewController.swift
@@ -15,6 +15,15 @@ class StatisticsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        monthYearInputPickerView = UIView(frame: CGRect(x: 0, y: 100, width: 320, height: 130))
+        monthYearPicker = UIPickerView(frame: CGRect(x: 0, y: 0, width: 320, height: 130))
+        monthYearPicker.delegate = self
+        monthYearPicker.dataSource = self
+        monthYearInputPickerView.addSubview(monthYearPicker)
+        oneMonthTextField.inputView = monthYearInputPickerView
+        beginningTimeTextField.inputView = monthYearInputPickerView
+        endingTimeTextField.inputView = monthYearInputPickerView
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,12 +31,44 @@ class StatisticsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK - Outlet
     @IBOutlet weak var oneMonthTextField: UITextField!
     @IBOutlet weak var oneMonthPieChart: PieChartView!
     @IBOutlet weak var beginningTimeTextField: UITextField!
     @IBOutlet weak var endingTimeTextField: UITextField!
     @IBOutlet weak var overviewBarChart: BarChartView!
-    @IBOutlet var tapGestureRecognizer: UITapGestureRecognizer!
     
+    var monthYearInputPickerView: UIView!
+    var monthYearPicker: UIPickerView!
+    
+    // MARK - Action
+    @IBAction func onTapRegconized(_ sender: UITapGestureRecognizer) {
+        oneMonthTextField.resignFirstResponder()
+        beginningTimeTextField.resignFirstResponder()
+        endingTimeTextField.resignFirstResponder()
+    }
+    
+}
 
+extension StatisticsViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return 12
+        } else {
+            return 10
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return "\(row + 1)"
+        } else {
+            return "\(row + 2018)"
+        }
+    }
+    
 }
--- moneytracker/Controller/SummaryViewController.swift
+++ moneytracker/Controller/SummaryViewController.swift
@@ -15,6 +15,24 @@ class SummaryViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Init picker view
+        typeCateInputPickerView = UIView(frame: CGRect(x: 0, y: 100, width: 320, height: 130))
+        typeCatePicker = UIPickerView(frame: CGRect(x: 0, y: 0, width: 320, height: 130))
+        typeCatePicker.delegate = self
+        typeCatePicker.dataSource = self
+        typeCateInputPickerView.addSubview(typeCatePicker)
+        typeCateTextField.inputView = typeCateInputPickerView
+        
+        monthYearInputPickerView = UIView(frame: CGRect(x: 0, y: 100, width: 320, height: 130))
+        monthYearPicker = UIPickerView(frame: CGRect(x: 0, y: 0, width: 320, height: 130))
+        monthYearPicker.delegate = self
+        monthYearPicker.dataSource = self
+        monthYearInputPickerView.addSubview(monthYearPicker)
+        beginningTimeTextField.inputView = monthYearInputPickerView
+        endingTimeTextField.inputView = monthYearInputPickerView
+        
+        cates = cateRef[0]
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +40,7 @@ class SummaryViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: Outlet
     @IBOutlet weak var balanceLabel: UILabel!
     @IBOutlet weak var typeCateTextField: UITextField!
     @IBOutlet weak var beginningTimeTextField: UITextField!
@@ -29,7 +48,86 @@ class SummaryViewController: UIViewController {
     @IBOutlet weak var budgetLabel: UILabel!
     @IBOutlet weak var changeBudgetButton: UIButton!
     @IBOutlet weak var activityTableView: UITableView!
-    @IBOutlet var tapGestureRecognizer: UITapGestureRecognizer!
     
+    var typeCateInputPickerView: UIView!
+    var typeCatePicker: UIPickerView!
+    var monthYearInputPickerView: UIView!
+    var monthYearPicker: UIPickerView!
+    
+    // MARK: Variable
+    let typeRef = ["All", "Income", "Expense"]
+    let cateRef = [["All"],["Salary", "Other"],["Food", "Travel", "Vehicle", "Utility", "Miscellaneous"]]
+    var cates: [String]!
+    
+    // MARK: Action
+    @IBAction func onTapRecognized(_ sender: UITapGestureRecognizer) {
+        typeCateTextField.resignFirstResponder()
+        beginningTimeTextField.resignFirstResponder()
+        endingTimeTextField.resignFirstResponder()
+    }
+    
+    
+    // MARK: Helper
+}
 
+extension SummaryViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == monthYearPicker {
+            if component == 0 {
+                return 12
+            } else {
+                return 10
+            }
+        } else {
+            if component == 0 {
+                return 3
+            } else {
+                return cates.count
+            }
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == monthYearPicker {
+            if component == 0 {
+                return "\(row + 1)"
+            } else {
+                return "\(row + 2018)"
+            }
+        } else {
+            if component == 0 {
+                return typeRef[row]
+            } else {
+                return cates[row]
+            }
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == typeCatePicker {
+            if component == 0 {
+                let selectedType = row
+                cates = cateRef[selectedType]
+                pickerView.reloadComponent(1)
+                pickerView.selectRow(0, inComponent: 1, animated: true)
+            }
+        }
+    }
+}
+
+extension SummaryViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "ActivityTableViewCell")
+        return cell
+    }
+    
+    
 }
