7c84db9353a304c104199b241832a93aa27fc7d8
Peter Ho <peterho@Peters-Mac.local>
2018-09-05 16:00:33 +0700

    Stretch UI

---
+++ moneytracker/Controller/ActivityTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActivityTableViewCell.swift
+//  moneytracker
+//
+//  Created by Peter Ho on 9/5/18.
+//  Copyright © 2018 Peter Ho. All rights reserved.
+//
+
+import UIKit
+
+class ActivityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ moneytracker/Controller/AddActivityViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AddActivityViewController.swift
+//  moneytracker
+//
+//  Created by Peter Ho on 9/5/18.
+//  Copyright © 2018 Peter Ho. All rights reserved.
+//
+
+import UIKit
+import Eureka
+
+class AddActivityViewController: FormViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
